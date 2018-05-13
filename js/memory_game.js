20a485a323578640288c555be1cbc964c81fa4dc
Gali Deutsch <deutsch.gali@gmail.com>
2018-05-13 08:26:34 +0300

    adding generate board function

--- js/memory_game.js
+++ js/memory_game.js
@@ -6,3 +6,17 @@ var MemoryGame = {};
         document.getElementById("message").style.display = "none";
     }
 
+MemoryGame.generateBoard = function(){
+    var cardArr = MemoryGame.generateArray(); 
+    var board = document.getElementById("board");
+    var board = document.getElementById("board");
+    for(var i = 0; i < cardArr.length; i++){
+        var card = document.createElement("div");
+        card.className = "card";
+        card.value = cardArr[i];
+        card.style.backgroundImage = "url('./images/card_backs.png')";
+        card.addEventListener("click", MemoryGame.cardClick);
+        board.appendChild(card);
+    }
+}
+
